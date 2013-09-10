e522f886d098c23b30ebf3e8a55f5571fb658987
Ashiq A. <ashes999@yahoo.com>
2013-09-10 12:41:22 -0400

    Hello World in ConsoleX

---
+++ Meltdown.Core/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleX;
+
+namespace Meltdown.Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainConsole console = MainConsole.Instance;
+            console.Color = Color.Red;
+            console.Write(0, 0, "RED");
+            console.Color = Color.Yellow;
+            console.Write(3, 0, "?!");
+            console.Refresh();
+            Console.ReadKey();
+
+        }
+    }
+}
